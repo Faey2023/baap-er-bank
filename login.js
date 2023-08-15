4e1b61544c651d92ac239acb7aaea906a221d235
Fahima <sfahima1388@gmail.com>
2023-08-15 06:37:04 +0600

    added input field js

--- login.js
+++ login.js
@@ -1,6 +1,15 @@
-//step 1 add click event handler
+//email setup
+//step 1- add click event handler
 document.getElementById("btn-submit").addEventListener("click", function () {
-  //get the email address
-  // always remember to get .value to get text from an input field.
+  //step 2-get the email address
+  //step 3- always remember to get .value to get text from an input field.
   const emailField = document.getElementById("email");
+  const email = emailField.value;
+  //password setup
+  const passField = document.getElementById("pass");
+  const pass = passField.value;
+  //step 4- verify email and password
+  if (email === "fahima2023@gmail.com" && pass === "secret") {
+    console.log("valid user");
+  }
 });
