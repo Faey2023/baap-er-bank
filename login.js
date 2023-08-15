5ceb9a444215d456555344eb2b330ea206b91884
Fahima <sfahima1388@gmail.com>
2023-08-15 06:26:15 +0600

    starting

---
+++ login.js
@@ -0,0 +1,6 @@
+//step 1 add click event handler
+document.getElementById("btn-submit").addEventListener("click", function () {
+  //get the email address
+  // always remember to get .value to get text from an input field.
+  const emailField = document.getElementById("email");
+});
